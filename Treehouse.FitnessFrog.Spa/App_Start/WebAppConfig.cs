941df27abe075fcd74593c56377bb481b3bfb1df
sangviehuy <hvsang2101@gmail.com>
2018-06-27 21:54:11 -0700

    Add project files.

---
+++ Treehouse.FitnessFrog.Spa/App_Start/WebAppConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Treehouse.FitnessFrog.Spa
+{
+    public static class WebAppConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+        }
+
+    }
+}

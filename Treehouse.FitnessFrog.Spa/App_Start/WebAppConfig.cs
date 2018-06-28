8f485e0133979cb8375d21c2f8a23fd15d65e266
sangviehuy <hvsang2101@gmail.com>
2018-06-27 22:10:42 -0700

    Add config route for WebApi.

--- Treehouse.FitnessFrog.Spa/App_Start/WebAppConfig.cs
+++ Treehouse.FitnessFrog.Spa/App_Start/WebAppConfig.cs
@@ -10,7 +10,11 @@ namespace Treehouse.FitnessFrog.Spa
     {
         public static void Register(HttpConfiguration config)
         {
-
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
         }
 
     }
